5ed3ac07a1d66b2bb7ae000916b33be9c3e43364
ArnoldOnchieku <kraftycoder8@gmail.com>
2025-07-03 16:37:40 +0300

    feat: add provider state management in the app

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -1,18 +1,25 @@
-import 'package:chat_app/chat_page.dart';
+import 'package:chat_app/services/auth_service.dart';
+import 'package:chat_app/utils/brand_color.dart';
+import 'package:chat_app/utils/spaces.dart';
+import 'package:chat_app/widgets/login_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:social_media_buttons/social_media_buttons.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({Key? key}) : super(key: key);
 
   final _formkey = GlobalKey<FormState>();
 
-  void loginUser(context) {
+  Future<void> loginUser(BuildContext context) async {
     if (_formkey.currentState != null && _formkey.currentState!.validate()) {
       print(userNameController.text);
       print(passwordController.text);
 
-      Navigator.push(
-          context, MaterialPageRoute(builder: (context) => ChatPage()));
+      await context.read<AuthService>().loginUser(userNameController.text);
+      Navigator.pushReplacementNamed(context, '/chat',
+          arguments: '${userNameController.text}');
       print('login successful!');
     } else {
       print('not successful!');
@@ -22,97 +29,161 @@ class LoginPage extends StatelessWidget {
   final userNameController = TextEditingController();
   final passwordController = TextEditingController();
 
+  final _mainUrl = "https://poojabhaumik.com";
+
+  Widget _buildHeader(context) {
+    return Column(
+      children: [
+        Text(
+          'Let\'s sign you in!',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: 30,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 0.5),
+        ),
+        Text(
+          'Welcome back! \n You\'ve been missed!',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 20,
+              color: Colors.blueGrey),
+        ),
+        verticalSpacing(24),
+        Container(
+          height: 200,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.fitWidth,
+                  image: AssetImage('assets/illustration.png')),
+              borderRadius: BorderRadius.circular(24)),
+        ),
+        verticalSpacing(24),
+      ],
+    );
+  }
+
+  Widget _buildFooter() {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () async {
+            print('Link clicked!');
+            if (!await launch(_mainUrl)) {
+              throw 'Could not launch this!';
+            }
+          },
+          child: Column(
+            children: [
+              Text('Find us on'),
+              Text(_mainUrl),
+            ],
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SocialMediaButton.twitter(
+                size: 20, url: "https://twitter.com/pooja_bhaumik"),
+            SocialMediaButton.linkedin(
+                size: 20, url: "https://linkedin.com/in/poojab26")
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildForm(context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              LoginTextField(
+                hintText: "Enter your username",
+                validator: (value) {
+                  if (value != null && value.isNotEmpty && value.length < 5) {
+                    return "Your username should be more than 5 characters";
+                  } else if (value != null && value.isEmpty) {
+                    return "Please type your username";
+                  }
+                  return null;
+                },
+                controller: userNameController,
+              ),
+              verticalSpacing(24),
+              LoginTextField(
+                hasAsterisks: true,
+                controller: passwordController,
+                hintText: 'Enter your password',
+              ),
+            ],
+          ),
+        ),
+        verticalSpacing(24),
+        ElevatedButton(
+            onPressed: () async {
+              await loginUser(context);
+            },
+            child: Text(
+              'Login',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
+            )),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    //TODO: Fix UI for web version
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                'Let\'s sign you in!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 0.5),
-              ),
-              Text(
-                'Welcome back! \n You\'ve been missed!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 20,
-                    color: Colors.blueGrey),
-              ),
-              Image.network(
-                'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-                height: 200,
-              ),
-              Form(
-                key: _formkey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      validator: (value) {
-                        if (value != null &&
-                            value.isNotEmpty &&
-                            value.length < 5) {
-                          return "Your username should be more than 5 characters";
-                        } else if (value != null && value.isEmpty) {
-                          return "Please type your username";
-                        }
-                        return null;
-                      },
-                      controller: userNameController,
-                      decoration: InputDecoration(
-                          hintText: 'Add your username',
-                          hintStyle: TextStyle(color: Colors.blueGrey),
-                          border: OutlineInputBorder()),
+          child: LayoutBuilder(builder: (context, BoxConstraints constraints) {
+            if (constraints.maxWidth > 1000) {
+              // web layout
+              return Row(
+                children: [
+                  Spacer(
+                    flex: 1,
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        _buildHeader(context),
+                        _buildFooter(),
+                      ],
                     ),
-                    SizedBox(
-                      height: 24,
-                    ),
-                    TextFormField(
-                      controller: passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          hintText: 'Type your password',
-                          hintStyle: TextStyle(color: Colors.blueGrey),
-                          border: OutlineInputBorder()),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    loginUser(context);
-                  },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
-                  )),
-              GestureDetector(
-                onTap: () {
-                  //todo: Navigate to browser
-                  print('Link clicked!');
-                },
-                child: Column(
-                  children: [
-                    Text('Find us on'),
-                    Text('https://poojabhaumik.com'),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  ),
+                  Spacer(
+                    flex: 1,
+                  ),
+                  Expanded(child: _buildForm(context)),
+                  Spacer(
+                    flex: 1,
+                  ),
+                ],
+              );
+            }
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                _buildHeader(context),
+                _buildForm(context),
+                _buildFooter(),
+              ],
+            );
+          }),
         ),
       ),
     );
--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:chat_app/chat_page.dart';
 import 'package:chat_app/login_page.dart';
+import 'package:chat_app/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(ChatApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AuthService.init();
+  runApp(ChangeNotifierProvider(
+    create: (BuildContext context) => AuthService(),
+    child: ChatApp(),
+  ));
 }
 
 class ChatApp extends StatelessWidget {
@@ -12,10 +19,23 @@ class ChatApp extends StatelessWidget {
     return MaterialApp(
       title: "Chat App",
       theme: ThemeData(
+          canvasColor: Colors.transparent,
           primarySwatch: Colors.deepPurple,
           appBarTheme: AppBarTheme(
               backgroundColor: Colors.blue, foregroundColor: Colors.black)),
-      home: LoginPage(),
+      home: FutureBuilder<bool>(
+          future: context.read<AuthService>().isLoggedIn(),
+          builder: (context, AsyncSnapshot<bool> snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasData && snapshot.data!) {
+                return ChatPage();
+              } else
+                return LoginPage();
+            }
+            return CircularProgressIndicator();
+          }),
+
+      routes: {'/chat': (context) => ChatPage()},
     );
   }
 }
